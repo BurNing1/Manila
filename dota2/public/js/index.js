9b64cd8ed28e0be69406359e7176aba6a67cfd8c
BurNing1 <h466238380@qq.com>
2016-04-25 14:56:29 +0800

    state

--- dota2/public/js/index.js
+++ dota2/public/js/index.js
@@ -46,34 +46,14 @@ $(function(){
 
   });
   $("#fileForm").submit(function(){
-    $("#_fileForm").ajaxSubmit({
+    $("#fileForm").ajaxSubmit({
       type: "post",
       url: "",
       success: function (data1) {
-        var remsg = data1.split("|");
-        var name = remsg[1].split("\/");
-        if (remsg[0] == "1") {
-          var type = name[4].substring(name[4].indexOf("."), name[4].length);
-          //$("#msg").html("文件名：" + name[name.length - 1] + "   ---  " + remsg[2]);
-          switch (type) {
-            case ".jpg":
-            case ".jpeg":
-            case ".gif":
-            case ".bmp":
-            case ".png":
-              $("img").attr({ "src": remsg[1] });
-              break;
-            default:
-              $("img").attr({ "src": "project/Images/msg_ok.png" });
-              break;
-          }
-        } else {
-          //$("#msg").html("文件上传失败：" + remsg[2]);
-          //$("img").attr({ "src": "project/Images/msg_error.png" });
-        }
+
       },
       error: function (msg) {
-        alert("文件上传失败");
+
       }
     });
     return false;
